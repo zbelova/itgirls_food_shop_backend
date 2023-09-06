20d28b1d9753ed966578b5dce49a84da5b0b74ed
daria-nutcracker <dsshchelokova.42@gmail.com>
2023-09-06 22:14:07 +0300

    b6 Создала UserUpdate RegistrationController

---
+++ src/main/java/ru/Product/controller/RegistrationController.java
@@ -0,0 +1,19 @@
+package ru.Product.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.context.request.WebRequest;
+import ru.Product.dto.UserDto;
+
+@Controller
+@RequiredArgsConstructor
+public class RegistrationController {
+    @GetMapping("/user/registration")
+    public String showRegistrationForm(WebRequest request, Model model) {
+        UserDto userDto = new UserDto();
+        model.addAttribute("user", userDto);
+        return "registration";
+    }
+}
