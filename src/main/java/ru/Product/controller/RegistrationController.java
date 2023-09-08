6fd3058f5478ad95868713e16d07143777cb32c3
daria-nutcracker <dsshchelokova.42@gmail.com>
2023-09-08 15:30:33 +0300

    b6 добавила в pom file spring-boot-starter-security

--- src/main/java/ru/Product/controller/RegistrationController.java
+++ src/main/java/ru/Product/controller/RegistrationController.java
@@ -10,10 +10,21 @@ import ru.Product.dto.UserDto;
 @Controller
 @RequiredArgsConstructor
 public class RegistrationController {
+
+
+//    login(String email, String password) - авторизация
+//    signUp(User user) - регистрация. Сейчас используется простая регистрация через почтовый ящик, подтверждения адреса почты не
+
     @GetMapping("/user/registration")
     public String showRegistrationForm(WebRequest request, Model model) {
         UserDto userDto = new UserDto();
         model.addAttribute("user", userDto);
         return "registration";
     }
+
+    //    updateUser(User user) - редактировать данные пользователя. Почтовый ящик сейчас поменять нельзя, потому что подтвердить изменение нельзя через почту. Пароль тоже нельзя. Но можем сделать просто смену почтового адреса и пароля в форме
+
+
+//    logout() - разлогиниться
+//    getUser() - получить данные текущего авторизованного пользователя
 }
