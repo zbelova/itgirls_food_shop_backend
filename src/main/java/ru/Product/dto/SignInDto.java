692570f1d4289a6a6127b3880294132f3f0448c4
daria-nutcracker <dsshchelokova.42@gmail.com>
2023-09-11 21:38:29 +0300

    b6 добавила комментарии @Schema @Operation

--- src/main/java/ru/Product/dto/SignInDto.java
+++ src/main/java/ru/Product/dto/SignInDto.java
@@ -1,5 +1,6 @@
 package ru.Product.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -12,9 +13,20 @@ import lombok.NoArgsConstructor;
 @Data
 @Builder
 public class SignInDto {
+    @Schema(
+            description = "user's email",
+            name = "email",
+            type = "string",
+            example = "example@gmail.com")
     @NotNull
     @NotEmpty
     private String email;
+
+    @Schema(
+            description = "password to sign in",
+            name = "password",
+            type = "string",
+            example = "password")
     @NotNull
     @NotEmpty
     private String password;
