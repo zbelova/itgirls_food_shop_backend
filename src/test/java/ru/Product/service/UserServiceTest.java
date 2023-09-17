48ae18ca73ae577b25cb2eefb9c4a4f2908950a6
daria-nutcracker <dsshchelokova.42@gmail.com>
2023-09-17 09:26:53 +0300

    b8 testUpdateUser() рабочий

--- src/test/java/ru/Product/service/UserServiceTest.java
+++ src/test/java/ru/Product/service/UserServiceTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.invocation.InvocationOnMock;
 import org.springframework.boot.test.context.SpringBootTest;
 import ru.Product.dto.UserDto;
@@ -97,8 +98,8 @@ public class UserServiceTest {
     public void testUpdateUser() {
         UUID id =  UUID.randomUUID();
         String name = "Daria";
-        String email = "daria@yandex.ru";
-        String phone = "8999666554";
+        String email = "daria@gmail.com";
+        String phone = "89996665544";
         String address = "Smolensk";
         String password = "PASSword8!";
         Set<Order> orders = new HashSet<>();
@@ -106,10 +107,12 @@ public class UserServiceTest {
         User user = new User (id, name, email, phone, address, password, orders);
 
         when(userRepository.findUserByName(name)).thenReturn(Optional.of(user));
+        when(userRepository.save(Mockito.any())).thenReturn(user);
 
         UserDto userDto = userService.updateUser(new UserUpdateDto(id, name, phone, address));
 
         verify(userRepository).findUserByName(name);
+        verify(userRepository).save(Mockito.any());
         Assertions.assertEquals(userDto.getId(),user.getId());
         Assertions.assertEquals(userDto.getName(),user.getName());
         Assertions.assertEquals(userDto.getPhone(),user.getPhone());
