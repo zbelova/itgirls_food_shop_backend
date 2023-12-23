ca992a1070bb883c34d0b0ef3b128e781e8b604f
MariiaBuruk <tyk.tykva@yandex.ru>
2023-12-23 15:38:12 +0300

    feature: 23-13-2023 15:40 User role upp

--- src/main/java/ru/Product/config/SecurityConfig.java
+++ src/main/java/ru/Product/config/SecurityConfig.java
@@ -26,27 +26,48 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         HttpBasicConfigurer<HttpSecurity> httpSecurityHttpBasicConfigurer = httpSecurity.csrf().disable().
                 authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("/api/v1/category/getOne").hasRole("USER")
+                        authorize.requestMatchers("/api/v1/category/getOne").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers("/api/v1/category/getAll").hasAnyRole("ADMIN", "USER")
 
-                                .requestMatchers("/api/v1/order/getAllOrders").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/product/getAllProducts").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers("/api/v1/product/getOneProduct").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers("/api/v1/product/getAllProductsFromOneCategory").hasAnyRole("ADMIN", "USER")
+
+                                .requestMatchers("api/v1/user/logout").hasAnyRole("ADMIN", "USER")
+
+                                .requestMatchers("/api/v1/order/getAllOrdersByUserId").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers("/api/v1/order/getOrderById").hasAnyRole("ADMIN", "USER")
+
+                                .requestMatchers("api/v1/user/getUser").hasRole("ADMIN")
 
                                 .requestMatchers("/api/v1/category/create").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/category/update").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/category/delete").hasRole("ADMIN")
+
                                 .requestMatchers("/api/v1/product/create").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/product/update").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/product/delete").hasRole("ADMIN")
 
-                                .requestMatchers("/api/v1/order/getAllOrdersByUserId").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/order/getAllOrders").hasRole("ADMIN")
-                                .requestMatchers("/api/v1/order/getOrderById").hasRole("ADMIN")
+                                .requestMatchers("/api/v1/order/getAllOrders").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/order/updateProductQuantityInOrder").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/order/addProductToOrder").hasRole("ADMIN")
                                 .requestMatchers("/api/v1/order/removeProductFromOrder").hasRole("ADMIN")
 
                                 .requestMatchers("api/name/orderStatus/{name}").hasRole("ADMIN")
                                 .requestMatchers("api/name/orderStatus/getAll").hasRole("ADMIN")
+
+                                .requestMatchers("/api/v1/cart/addProductToCart").hasRole("USER")
+                                .requestMatchers("/api/v1/cart/removeProductFromCart").hasRole("USER")
+                                .requestMatchers("/api/v1/cart/getCart").hasRole("USER")
+                                .requestMatchers("/api/v1/cart/removeAllProductsFromCart").hasRole("USER")
+                                .requestMatchers("/api/v1/cart/getProductsInStock").hasRole("USER")
+
+                                .requestMatchers("/api/v1/order/saveOrder").hasRole("USER")
+                                .requestMatchers("api/v1/user/updateUser").hasRole("USER")
+
                                 .anyRequest().authenticated())
+
                 .httpBasic();
         return httpSecurity.build();
     }
